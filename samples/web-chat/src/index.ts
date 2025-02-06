6286d8deadc448780f9b7052dd7e40e6c35059b8
Alejandro Colman <alejandro.colman@southworks.com>
2025-02-06 17:45:57 -0300

    feat: add MultiFeatureBot and proactive messaging endpoint

--- samples/web-chat/src/index.ts
+++ samples/web-chat/src/index.ts
@@ -2,13 +2,17 @@
 // Licensed under the MIT License.
 
 import express, { Response } from 'express'
-
 import rateLimit from 'express-rate-limit'
+
 import { Request, CloudAdapter, authorizeJWT, AuthConfiguration, loadAuthConfigFromEnv } from '@microsoft/agents-bot-hosting'
+import { ConversationReference } from '@microsoft/agents-activity-schema'
+
 import { AdaptiveCardBot } from './01.adaptiveCardsBot'
 import { CardFactoryBot } from './02.cardFactoryBot'
+import { MultiFeatureBot } from './03.multiFeatureBot'
 
 const authConfig: AuthConfiguration = loadAuthConfigFromEnv()
+const conversationReferences: { [key: string]: ConversationReference } = {}
 
 const createBot = (botName: string) => {
   switch (botName) {
@@ -16,6 +20,8 @@ const createBot = (botName: string) => {
       return new AdaptiveCardBot()
     case 'CardFactoryBot':
       return new CardFactoryBot()
+    case 'MultiFeatureBot':
+      return new MultiFeatureBot(conversationReferences)
     default:
       throw new Error(`Bot with name ${botName} is not recognized.`)
   }
@@ -31,6 +37,19 @@ const app = express()
 app.use(rateLimit({ validate: { xForwardedForHeader: false } }))
 app.use(express.json())
 app.use(authorizeJWT(authConfig))
+
+app.get('/api/notify', async (_req: Request, res: Response) => {
+  for (const conversationReference of Object.values(conversationReferences)) {
+    await adapter.continueConversation(conversationReference, async context => {
+      await context.sendActivity('proactive hello')
+    })
+  }
+
+  res.setHeader('Content-Type', 'text/html')
+  res.writeHead(200)
+  res.write('<html><body><h1>Proactive messages have been sent.</h1></body></html>')
+  res.end()
+})
 
 app.post('/api/messages', async (req: Request, res: Response) => {
   await adapter.process(req, res, async (context) => await myBot.run(context))
