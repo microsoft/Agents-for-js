822635c2a77de258a41840996c798a29c5d48119
Rido <rido-min@users.noreply.github.com>
2025-02-27 23:23:54 +0000

    add comments to teams package

--- packages/agents-bot-hosting-teams/src/oauth/teamsUserTokenClient.ts
+++ packages/agents-bot-hosting-teams/src/oauth/teamsUserTokenClient.ts
@@ -5,7 +5,18 @@ import { debug, UserTokenClient, TokenExchangeRequest } from '@microsoft/agents-
 
 const logger = debug('agents:teamsUserTokenClient')
 
+/**
+ * TeamsUserTokenClient is responsible for handling token exchange operations for Teams users.
+ */
 export class TeamsUserTokenClient extends UserTokenClient {
+  /**
+   * Exchanges a token for a user.
+   * @param userId The ID of the user.
+   * @param connectionName The name of the connection.
+   * @param channelId The ID of the channel.
+   * @param tokenExchangeRequest The token exchange request.
+   * @returns A promise that resolves to the exchanged token or null if an error occurs.
+   */
   async exchangeTokenAsync (userId: string, connectionName: string, channelId: string, tokenExchangeRequest: TokenExchangeRequest) {
     try {
       const params = { userId, connectionName, channelId }
