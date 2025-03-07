ad1f55b957e39bc825a8788d81103ab768dc646d
VictoriaAcqua <victoria.acqua@southworks.com>
2025-03-07 11:06:17 -0300

    Remove unused MemoryFork

--- packages/agents-bot-hosting/src/app/memoryFork.ts
+++ packages/agents-bot-hosting/src/app/memoryFork.ts
@@ -3,8 +3,6 @@
  * Licensed under the MIT License.
  */
 
-const TEMP_SCOPE = 'temp'
-
 export interface Memory {
   deleteValue(path: string): void;
 
@@ -13,63 +11,4 @@ export interface Memory {
   getValue<TValue = unknown>(path: string): TValue;
 
   setValue(path: string, value: unknown): void;
-}
-
-export class MemoryFork implements Memory {
-  private readonly _fork: Record<string, Record<string, unknown>> = {}
-  private readonly _memory: Memory
-
-  public constructor (memory: Memory) {
-    this._memory = memory
-  }
-
-  public deleteValue (path: string): void {
-    const { scope, name } = this.getScopeAndName(path)
-    if (
-      Object.prototype.hasOwnProperty.call(this._fork, scope) &&
-            Object.prototype.hasOwnProperty.call(this._fork[scope], name)
-    ) {
-      delete this._fork[scope][name]
-    }
-  }
-
-  public hasValue (path: string): boolean {
-    const { scope, name } = this.getScopeAndName(path)
-    if (Object.prototype.hasOwnProperty.call(this._fork, scope)) {
-      return Object.prototype.hasOwnProperty.call(this._fork[scope], name)
-    } else {
-      return this._memory.hasValue(path)
-    }
-  }
-
-  public getValue<TValue = unknown>(path: string): TValue {
-    const { scope, name } = this.getScopeAndName(path)
-    if (Object.prototype.hasOwnProperty.call(this._fork, scope)) {
-      if (Object.prototype.hasOwnProperty.call(this._fork[scope], name)) {
-        return this._fork[scope][name] as TValue
-      }
-    }
-
-    return this._memory.getValue(path)
-  }
-
-  public setValue (path: string, value: unknown): void {
-    const { scope, name } = this.getScopeAndName(path)
-    if (!Object.prototype.hasOwnProperty.call(this._fork, scope)) {
-      this._fork[scope] = {}
-    }
-
-    this._fork[scope][name] = value
-  }
-
-  private getScopeAndName (path: string): { scope: string; name: string } {
-    const parts = path.split('.')
-    if (parts.length > 2) {
-      throw new Error(`Invalid state path: ${path}`)
-    } else if (parts.length === 1) {
-      parts.unshift(TEMP_SCOPE)
-    }
-
-    return { scope: parts[0], name: parts[1] }
-  }
 }
