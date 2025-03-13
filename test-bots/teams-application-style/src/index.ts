09bba6fef26ea1b178be92d91e1b9ae5f40d918e
VictoriaAcqua <victoria.acqua@southworks.com>
2025-03-13 17:11:32 -0300

    WIP add teams app sample

---
+++ test-bots/teams-application-style/src/index.ts
@@ -0,0 +1,53 @@
+import { AuthConfiguration, authorizeJWT, loadAuthConfigFromEnv, Request } from '@microsoft/agents-bot-hosting'
+import { TeamsCloudAdapter } from '@microsoft/agents-bot-hosting-teams'
+import express, { Response } from 'express'
+import rateLimit from 'express-rate-limit'
+import path from 'path'
+
+const authConfig: AuthConfiguration = loadAuthConfigFromEnv()
+const adapter = new TeamsCloudAdapter(authConfig)
+
+const server = express()
+server.use(rateLimit({ validate: { xForwardedForHeader: false } }))
+server.use(express.json())
+server.use(authorizeJWT(authConfig))
+
+async function loadModule () {
+  const moduleName = process.env.botName || 'TeamsJsBot'
+  let module
+  switch (moduleName) {
+    case 'TeamsJsBot':
+      module = (await import('./teamsJsBot')).app
+      return module
+    default:
+      throw new Error(`Bot with name ${moduleName} is not recognized.`)
+  }
+}
+
+server.use(express.static(path.join(__dirname, '..', 'public')))
+
+server.get('/Youtube', (_req, res) => {
+  const filePath = path.join(__dirname, '../pages/youtube.html')
+  res.sendFile(filePath)
+})
+
+server.get('/CustomForm', (_req, res) => {
+  const filePath = path.join(__dirname, '../pages/customForm.html')
+  res.sendFile(filePath)
+})
+
+server.post('/CustomForm', (_req) => {
+  console.log('Data is being sent to the teams handler when this endpoint is called by teams')
+})
+
+server.post('/api/messages', async (req: Request, res: Response) => {
+  await adapter.process(req, res, async (context) => {
+    const app = await loadModule()
+    await app.run(context)
+  })
+})
+
+const port = process.env.PORT || 3978
+server.listen(port, () => {
+  console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`)
+})
