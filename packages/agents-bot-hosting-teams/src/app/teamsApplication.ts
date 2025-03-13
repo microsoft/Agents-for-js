73c523b9b425ecfdb185383cc329a3a4cfaf6371
VictoriaAcqua <victoria.acqua@southworks.com>
2025-03-13 10:15:42 -0300

    Add teams app handlers

--- packages/agents-bot-hosting-teams/src/app/teamsApplication.ts
+++ packages/agents-bot-hosting-teams/src/app/teamsApplication.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { ActivityTypes, AgentApplication, AppRoute, debug, RouteSelector, TurnContext, TurnState } from '@microsoft/agents-bot-hosting'
+import { Activity, ActivityTypes, AgentApplication, AppRoute, ConversationReference, debug, RouteSelector, TurnContext, TurnState } from '@microsoft/agents-bot-hosting'
 import { TeamsApplicationOptions } from './teamsApplicationOptions'
 import { RouteHandler } from '@microsoft/agents-bot-hosting/dist/src/app/routeHandler'
 import { AdaptiveCards } from './adaptiveCards'
@@ -11,6 +11,15 @@ import { Messages } from './messages'
 import { MessageExtensions } from './messageExtensions'
 import { Meetings } from './meetings'
 import { TaskModules } from './taskModules'
+import { TeamsMessageEvents } from './teamsMessageEvents'
+import { MessageReactionEvents } from './messageReactionEvents'
+import { FileConsentCardResponse } from '../file/fileConsentCardResponse'
+import { ChannelInfo } from '../channel-data/channelInfo'
+import { TeamsInfo } from '../teamsInfo'
+import { TeamDetails } from '../connector-client/teamDetails'
+import { TeamsPagedMembersResult } from '../connector-client/teamsPagedMembersResult'
+import { ReadReceiptInfo } from '../message-read-info/readReceipInfo'
+import { TeamsConversationUpdateEvents } from './teamsConversationUpdateEvents'
 
 const logger = debug('agents:teams-application')
 
@@ -145,5 +154,295 @@ export class TeamsApplication<TState extends TurnState> extends AgentApplication
         this.stopTypingTimer()
       }
     })
+  }
+
+  public conversationUpdate (
+    event: TeamsConversationUpdateEvents,
+    handler: (context: TurnContext, state: TState) => Promise<void>
+  ): this {
+    if (typeof handler !== 'function') {
+      throw new Error(
+            `ConversationUpdate 'handler' for ${event} is ${typeof handler}. Type of 'handler' must be a function.`
+      )
+    }
+
+    const selector = this.createTeamsConversationUpdateSelector(event)
+    this.addRoute(selector, handler)
+    return this
+  }
+
+  public messageEventUpdate (
+    event: TeamsMessageEvents,
+    handler: (context: TurnContext, state: TState) => Promise<void>
+  ): this {
+    if (typeof handler !== 'function') {
+      throw new Error(
+              `MessageUpdate 'handler' for ${event} is ${typeof handler}. Type of 'handler' must be a function.`
+      )
+    }
+
+    const selector = this.createMessageEventUpdateSelector(event)
+    this.addRoute(selector, handler)
+    return this
+  }
+
+  public messageReactions (
+    event: MessageReactionEvents,
+    handler: (context: TurnContext, state: TState) => Promise<void>
+  ): this {
+    const selector = this.createMessageReactionSelector(event)
+    this.addRoute(selector, handler)
+    return this
+  }
+
+  public fileConsentAccept (
+    handler: (context: TurnContext, state: TState, fileConsentResponse: FileConsentCardResponse) => Promise<void>
+  ): this {
+    const selector = (context: TurnContext): Promise<boolean> => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+                context.activity.name === 'fileConsent/invoke' &&
+                context.activity.value?.action === 'accept'
+      )
+    }
+    const handlerWrapper = async (context: TurnContext, state: TState) => {
+      await handler(context, state, context.activity.value as FileConsentCardResponse)
+      await context.sendActivity({
+        type: ActivityTypes.InvokeResponse,
+        value: { status: 200 }
+      } as Activity)
+    }
+    this.addRoute(selector, handlerWrapper, true)
+    return this
+  }
+
+  public fileConsentDecline (
+    handler: (context: TurnContext, state: TState, fileConsentResponse: FileConsentCardResponse) => Promise<void>
+  ): this {
+    const selector = (context: TurnContext): Promise<boolean> => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+                context.activity.name === 'fileConsent/invoke' &&
+                context.activity.value?.action === 'decline'
+      )
+    }
+    const handlerWrapper = async (context: TurnContext, state: TState) => {
+      await handler(context, state, context.activity.value as FileConsentCardResponse)
+      await context.sendActivity({
+        type: ActivityTypes.InvokeResponse,
+        value: { status: 200 }
+      } as Activity)
+    }
+    this.addRoute(selector, handlerWrapper, true)
+    return this
+  }
+
+  public handoff (handler: (context: TurnContext, state: TState, continuation: string) => Promise<void>): this {
+    const selector = (context: TurnContext): Promise<boolean> => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke && context.activity.name === 'handoff/action'
+      )
+    }
+    const handlerWrapper = async (context: TurnContext, state: TState) => {
+      await handler(context, state, context.activity.value!.continuation)
+      await context.sendActivity({
+        type: ActivityTypes.InvokeResponse,
+        value: { status: 200 }
+      } as Activity)
+    }
+    this.addRoute(selector, handlerWrapper, true)
+    return this
+  }
+
+  public async getTeamChannels (
+    context: TurnContext | ConversationReference | Activity
+  ): Promise<ChannelInfo[]> {
+    let teamsChannels: ChannelInfo[] = []
+
+    const reference: ConversationReference = this.getConversationReference(context)
+
+    if (reference.conversation?.conversationType === 'channel') {
+      await this.continueConversationAsync(reference, async (ctx) => {
+        const teamId =
+                ctx.activity?.channelData?.team?.id ??
+                (ctx.activity?.conversation?.name === undefined ? ctx.activity?.conversation?.id : undefined)
+        if (teamId) {
+          teamsChannels = await TeamsInfo.getTeamChannels(ctx, teamId)
+        }
+      })
+    }
+
+    return teamsChannels
+  }
+
+  public async getTeamDetails (
+    context: TurnContext | ConversationReference | Activity
+  ): Promise<TeamDetails | undefined> {
+    let teamDetails: TeamDetails | undefined
+
+    const reference: ConversationReference = this.getConversationReference(context)
+
+    if (reference.conversation?.conversationType === 'channel') {
+      await this.continueConversationAsync(reference, async (ctx) => {
+        const teamId =
+                ctx.activity?.channelData?.team?.id ??
+                (ctx.activity?.conversation?.name === undefined ? ctx.activity?.conversation?.id : undefined)
+        if (teamId) {
+          teamDetails = await TeamsInfo.getTeamDetails(ctx, teamId)
+        }
+      })
+    }
+
+    return teamDetails
+  }
+
+  public async getPagedMembers (
+    context: TurnContext | ConversationReference,
+    pageSize?: number,
+    continuationToken?: string
+  ): Promise<TeamsPagedMembersResult> {
+    let pagedMembers: TeamsPagedMembersResult = { members: [], continuationToken: '' }
+    await this.continueConversationAsync(context, async (ctx) => {
+      pagedMembers = await TeamsInfo.getPagedMembers(ctx, pageSize, continuationToken)
+    })
+
+    return pagedMembers
+  }
+
+  public teamsReadReceipt (
+    handler: (context: TurnContext, state: TState, readReceiptInfo: ReadReceiptInfo) => Promise<void>
+  ): this {
+    const selector = (context: TurnContext): Promise<boolean> => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Event &&
+                context.activity.channelId === 'msteams' &&
+                context.activity.name === 'application/vnd.microsoft/readReceipt'
+      )
+    }
+
+    const handlerWrapper = (context: TurnContext, state: TState): Promise<void> => {
+      const readReceiptInfo = context.activity.value as ReadReceiptInfo
+      return handler(context, state, readReceiptInfo)
+    }
+
+    this.addRoute(selector, handlerWrapper)
+
+    return this
+  }
+
+  private createMessageEventUpdateSelector (event: TeamsMessageEvents): RouteSelector {
+    switch (event) {
+      case 'editMessage':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.MessageUpdate &&
+                        context?.activity?.channelData?.eventType === event
+          )
+        }
+      case 'softDeleteMessage':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.MessageDelete &&
+                        context?.activity?.channelData?.eventType === event
+          )
+        }
+      case 'undeleteMessage':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.MessageUpdate &&
+                        context?.activity?.channelData?.eventType === event
+          )
+        }
+      default:
+        throw new Error(`Invalid TeamsMessageEvent type: ${event}`)
+    }
+  }
+
+  private createMessageReactionSelector (event: MessageReactionEvents): RouteSelector {
+    switch (event) {
+      case 'reactionsAdded':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.MessageReaction &&
+                        Array.isArray(context?.activity?.reactionsAdded) &&
+                        context.activity.reactionsAdded.length > 0
+          )
+        }
+      case 'reactionsRemoved':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.MessageReaction &&
+                        Array.isArray(context?.activity?.reactionsRemoved) &&
+                        context.activity.reactionsRemoved.length > 0
+          )
+        }
+    }
+  }
+
+  private getConversationReference (
+    context: TurnContext | Activity | ConversationReference
+  ): ConversationReference {
+    let reference: ConversationReference
+    if (typeof (context as TurnContext).activity === 'object') {
+      reference = (context as TurnContext).activity.getConversationReference()
+    } else if (typeof (context as Activity).type === 'string') {
+      reference = (context as Activity).getConversationReference()
+    } else {
+      reference = context as ConversationReference
+    }
+    return reference
+  }
+
+  private createTeamsConversationUpdateSelector (event: TeamsConversationUpdateEvents): RouteSelector {
+    switch (event) {
+      case 'channelCreated':
+      case 'channelDeleted':
+      case 'channelRenamed':
+      case 'channelRestored':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.ConversationUpdate &&
+                        context?.activity?.channelData?.eventType === event &&
+                        context?.activity?.channelData?.channel &&
+                        context.activity.channelData?.team
+          )
+        }
+      case 'membersAdded':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.ConversationUpdate &&
+                        Array.isArray(context?.activity?.membersAdded) &&
+                        context.activity.membersAdded.length > 0
+          )
+        }
+      case 'membersRemoved':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.ConversationUpdate &&
+                        Array.isArray(context?.activity?.membersRemoved) &&
+                        context.activity.membersRemoved.length > 0
+          )
+        }
+      case 'teamRenamed':
+      case 'teamDeleted':
+      case 'teamHardDeleted':
+      case 'teamArchived':
+      case 'teamUnarchived':
+      case 'teamRestored':
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.ConversationUpdate &&
+                        context?.activity?.channelData?.eventType === event &&
+                        context?.activity?.channelData?.team
+          )
+        }
+      default:
+        return (context: TurnContext) => {
+          return Promise.resolve(
+            context?.activity?.type === ActivityTypes.ConversationUpdate &&
+                        context?.activity?.channelData?.eventType === event
+          )
+        }
+    }
   }
 }
