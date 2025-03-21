d22a70e336409f76f34e24cbcd5c7b2aba4fa363
VictoriaAcqua <victoria.acqua@southworks.com>
2025-03-21 14:47:51 -0300

    WIP teamsMultiFeatureBot

---
+++ test-bots/teams-application-style/src/teamsMultiFeatureBot.ts
@@ -0,0 +1,335 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import {
+  ActionTypes,
+  ActivityTypes,
+  Attachment,
+  CardAction,
+  CardFactory,
+  CardImage,
+  HeroCard,
+  MessageFactory,
+  TaskModuleAction,
+  ThumbnailCard,
+  TurnContext,
+  TurnState
+} from '@microsoft/agents-bot-hosting'
+import { MessagingExtensionAttachment, TeamsApplication, TeamsInfo, MessagingExtensionResult } from '@microsoft/agents-bot-hosting-teams'
+import { TaskModuleUIConstants } from './models/taskModuleUIConstants'
+import { CardTaskFetchValue } from './models/cardTaskFetchValue'
+// const adaptiveCardResource = require('../cards/AdaptiveCard.json')
+const restaurantCardResource = require('./cards/RestaurantCard.json')
+
+// const baseUrl = process.env.BASE_URL?.endsWith('/') ? process.env.BASE_URL : process.env.BASE_URL + '/'
+
+type ApplicationTurnState = TurnState
+export const app = new TeamsApplication()
+
+app.messageEventUpdate('editMessage', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('You edited a message')
+  await context.sendActivity(reply)
+})
+
+app.messageEventUpdate('softDeleteMessage', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('You edited a message')
+  await context.sendActivity(reply)
+})
+
+app.messageEventUpdate('undeleteMessage', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('You deleted a message')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('membersAdded', async (context: TurnContext, state: ApplicationTurnState) => {
+  const newMember = JSON.stringify(context.activity.membersAdded)
+  const reply = MessageFactory.text('Hi there! You are a new member' + newMember)
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('membersRemoved', async (context: TurnContext, state: ApplicationTurnState) => {
+  const removedMember = JSON.stringify(context.activity.membersRemoved)
+  const reply = MessageFactory.text('Hi there! A team member was removed' + removedMember)
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('teamRenamed', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the team was renamed')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('teamArchived', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the team was archived')
+  console.log('Hi, the team was archived')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('teamDeleted', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the team was deleted')
+  console.log('Hi, the team was deleted')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('teamHardDeleted', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the team was hard deleted')
+  console.log('Hi, the team was hard deleted')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('teamRestored', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the team has been restored')
+  console.log('Hi, the team has been restored')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('teamUnarchived', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the team has been unarchived')
+  console.log('Hi, the team has been unarchived')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('channelCreated', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the channel has been created')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('channelDeleted', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the channel has been deleted')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('channelRenamed', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the channel has been renamed')
+  await context.sendActivity(reply)
+})
+
+app.conversationUpdate('channelRestored', async (context: TurnContext, state: ApplicationTurnState) => {
+  const reply = MessageFactory.text('Hi, the channel has been restored')
+  console.log('Hi, the channel has been restored')
+  await context.sendActivity(reply)
+})
+
+app.messageExtensions.selectItem(async (context: TurnContext, state: ApplicationTurnState, _query: any) => {
+  const packageId = _query['packageId']
+  const version = _query['version']
+  const description = _query['description']
+  const projectUrl = _query['projectUrl']
+  const iconUrl = _query['iconUrl']
+
+  const buttons: CardAction[] = [
+    {
+      type: ActionTypes.OpenUrl,
+      title: 'Nuget Package',
+      value: `https://www.nuget.org/packages/${packageId}`
+    },
+    {
+      type: ActionTypes.OpenUrl,
+      title: 'Project',
+      value: projectUrl
+    }
+  ]
+
+  const card: Partial<ThumbnailCard> = {
+    title: `${packageId}, ${version}`,
+    subtitle: description,
+    buttons
+  }
+
+  if (iconUrl) {
+    card.images = [{
+      url: iconUrl,
+      alt: 'Icon'
+    }]
+  }
+
+  const attachment: MessagingExtensionAttachment = {
+    contentType: 'application/vnd.microsoft.card.thumbnail',
+    content: card
+  }
+
+  return {
+    attachmentLayout: 'list',
+    type: 'result',
+    attachments: [attachment]
+  }
+})
+
+app.messageExtensions.query('', async (context: TurnContext, state: ApplicationTurnState, query: any) => {
+  let text = ''
+
+  if (context.activity.value) {
+    if (query?.parameters?.at(0)?.name === 'initialRun') {
+      return {}
+    }
+
+    text = query?.parameters?.at(0)?.value ?? ''
+    switch (text) {
+      case 'link':
+      {
+        const card: Partial<HeroCard> = {
+          title: 'This is a Link Unfurling Sample',
+          subtitle: 'It will unfurl links from *.BotFramework.com',
+          text: 'This sample demonstrates how to handle link unfurling in Teams.'
+        }
+        const attachment: Attachment = {
+          content: card,
+          contentType: CardFactory.contentTypes.heroCard
+        }
+        return {
+          attachmentLayout: 'list',
+          type: 'result',
+          attachments: [{
+            content: card,
+            contentType: CardFactory.contentTypes.heroCard,
+            preview: attachment
+          }]
+        }
+      }
+      case 'adaptive card': {
+        const response: MessagingExtensionResult = getAdaptiveCard()
+        return response
+      }
+    }
+  }
+
+  const packages = await findPackages(text)
+
+  // We take every row of the results and wrap them in cards wrapped in MessagingExtensionAttachment objects.
+  // The Preview is optional, if it includes a Tap, that will trigger the OnTeamsMessagingExtensionSelectItem event back on this bot.
+  const attachments: MessagingExtensionAttachment[] = packages.map(({ item1, item2, item3, item4, item5 }) => {
+    const cardValue = `{"packageId": "${item1}", "version": "${item2}", "description": "${item3}", "projectUrl": "${item4}", "iconUrl": "${item5}"}`
+
+    const cardAction: CardAction = {
+      type: 'invoke',
+      value: cardValue,
+      title: item1
+    }
+    const previewCard: Partial<ThumbnailCard> = {
+      title: item1,
+      tap: cardAction
+    }
+
+    if (item5) {
+      const cardImage: CardImage = {
+        url: item5,
+        alt: 'icon'
+      }
+      previewCard.images = [cardImage]
+    }
+
+    const heroCard: Partial<HeroCard> = {
+      title: item1
+    }
+
+    const attachment: Attachment = {
+      content: previewCard,
+      contentType: 'application/vnd.microsoft.card.thumbnail'
+    }
+
+    const messagingExtensionAttachment: MessagingExtensionAttachment = {
+      contentType: CardFactory.contentTypes.heroCard,
+      content: heroCard,
+      preview: attachment
+    }
+
+    return messagingExtensionAttachment
+  })
+
+  return {
+    attachmentLayout: 'list',
+    type: 'result',
+    attachments
+  }
+})
+
+app.activity(ActivityTypes.Message, async (context: TurnContext, state: ApplicationTurnState) => {
+  if (context.activity.text?.indexOf('taskModule')! > 0) {
+    const reply = MessageFactory.attachment(getTaskModuleHeroCardOptions())
+    await context.sendActivity(reply)
+  } else if (context.activity.text?.indexOf('teamsinfo')! > 0) {
+    const channels = await TeamsInfo.getTeamChannels(context)
+    const msg1 = `Meeting Participant: ${JSON.stringify(channels)}}`
+    await context.sendActivity(MessageFactory.text(msg1))
+
+    const teamDetails = await TeamsInfo.getTeamDetails(context)
+    const msg2 = `Team Details: ${JSON.stringify(teamDetails)}`
+    await context.sendActivity(MessageFactory.text(msg2))
+  } else {
+    await context.sendActivity(MessageFactory.text('type teamsinfo or taskModule'))
+  }
+})
+
+function getTaskModuleHeroCardOptions (): Attachment {
+  const taskModuleActions: any = []
+  const taskModules = [
+    TaskModuleUIConstants.AdaptiveCard,
+    TaskModuleUIConstants.CustomForm,
+    TaskModuleUIConstants.YouTube
+  ]
+
+  taskModules.map((taskModule) => {
+    const stringFetchValue = new CardTaskFetchValue<string>()
+    stringFetchValue.data = taskModule.id
+
+    const taskModuleAction = new TaskModuleAction(taskModule.buttonTitle as string, stringFetchValue)
+
+    taskModuleActions.push(taskModuleAction)
+    return taskModuleAction
+  })
+
+  const heroCard: Partial<HeroCard> = {
+    title: 'Dialogs (referred to as task modules in TeamsJS v1.x) Invocation from Hero Card',
+    buttons: taskModuleActions
+  }
+
+  const attachment: Attachment = {
+    content: heroCard,
+    contentType: CardFactory.contentTypes.heroCard
+  }
+
+  return attachment
+}
+
+async function findPackages (text: string): Promise<[{ item1: string, item2: string, item3: string, item4: string, item5: string }]> {
+  const response = await fetch(`https://azuresearch-usnc.nuget.org/query?q=id:${text}&prerelease=true`)
+  const status = response.status
+
+  if (status !== 200) {
+    return [{ item1: '', item2: '', item3: '', item4: '', item5: '' }]
+  } else {
+    const jsonValues: any = await response.json()
+    const items = jsonValues['data']
+    return items.map((item: any) => {
+      return { item1: item['id'], item2: item['version'], item3: item['description'], item4: item['projectUrl'], item5: item['iconUrl'] }
+    })
+  }
+}
+
+function getAdaptiveCard (): MessagingExtensionResult {
+  const previewCard: Partial<ThumbnailCard> = {
+    title: 'Adaptive Card',
+    text: 'Please select to get Adaptive card'
+  }
+  const attachment: Attachment = {
+    contentType: 'application/vnd.microsoft.card.adaptive',
+    content: restaurantCardResource
+  }
+
+  const previewAttachment: Attachment = {
+    content: previewCard,
+    contentType: 'application/vnd.microsoft.card.thumbnail'
+  }
+
+  const messagingExtensionAttachment: MessagingExtensionAttachment = {
+    contentType: 'application/vnd.microsoft.card.adaptive',
+    content: attachment.content,
+    preview: previewAttachment
+  }
+
+  const messagingExtensionAttachmentList = [messagingExtensionAttachment]
+  return {
+    attachmentLayout: 'list',
+    type: 'result',
+    attachments: messagingExtensionAttachmentList
+  }
+}
