c2f20b61347e273e2bb66bd306b2e03c083b8a00
Rido <rido-min@users.noreply.github.com>
2025-04-02 16:51:32 +0000

    add wireCompat unit tests

--- packages/agents-activity/src/activityWireCompat.ts
+++ packages/agents-activity/src/activityWireCompat.ts
@@ -1,25 +1,6 @@
 // the property 'agent' is used in the activity wire protocol to refer to the bot
 
-// function rep (k: string, v: any) {
-//   if (typeof v === 'object' && v !== null && v['agent']) {
-//     const ov = v['agent']
-//     delete v['agent']
-//     v['bot'] = ov
-//   }
-//   return v
-// }
-
-// function rev (k: string, v: any) {
-//   if (typeof v === 'object' && v !== null && v['bot']) {
-//     const ov = v['bot']
-//     delete v['bot']
-//     v['agent'] = ov
-//   }
-//   return v
-// }
-
 export function normalizeIncomingPayload (payload: any): object {
-  // return JSON.parse(JSON.stringify(payload, rep), rev)
   if (payload['relatesTo'] && payload['relatesTo']['bot']) {
     const relatesTo = payload['relatesTo']
     const ov = relatesTo['bot']
@@ -30,7 +11,6 @@ export function normalizeIncomingPayload (payload: any): object {
 }
 
 export function normalizeOutgoingPayload (payload: any): object {
-  // return JSON.stringify(payload, rep)
   if (payload['relatesTo'] && payload['relatesTo']['agent']) {
     const relatesTo = payload['relatesTo']
     const ov = relatesTo['agent']
