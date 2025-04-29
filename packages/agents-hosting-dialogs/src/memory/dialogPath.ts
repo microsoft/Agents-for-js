662b41c6bea4de6ac0280ea3a6997ce004a0faed
Jim Daly <jim.daly@microsoft.com>
2025-04-29 16:31:34 -0700

    dialogPath comments

--- packages/agents-hosting-dialogs/src/memory/dialogPath.ts
+++ packages/agents-hosting-dialogs/src/memory/dialogPath.ts
@@ -7,27 +7,43 @@
  * Defines path for available dialogs.
  */
 export class DialogPath {
-  /// Counter of emitted events.
-  static readonly eventCounter = 'dialog.eventCounter'
+  /**
+   * Counter of emitted events.
+   */
+  static readonly eventCounter = "dialog.eventCounter";
+  /**
+   * Currently expected properties.
+   */
 
-  /// Currently expected properties.
-  static readonly expectedProperties = 'dialog.expectedProperties'
+  static readonly expectedProperties = "dialog.expectedProperties";
+  /**
+   * Default operation to use for entities where there is no identified operation entity.
+   */
 
-  /// Default operation to use for entities where there is no identified operation entity.
-  static readonly defaultOperation = 'dialog.defaultOperation'
+  static readonly defaultOperation = "dialog.defaultOperation";
+  /**
+   * Last surfaced entity ambiguity event.
+   */
 
-  /// Last surfaced entity ambiguity event.
-  static readonly lastEvent = 'dialog.lastEvent'
+  static readonly lastEvent = "dialog.lastEvent";
+  /**
+   * Currently required properties.
+   */
 
-  /// Currently required properties.
-  static readonly requiredProperties = 'dialog.requiredProperties'
+  static readonly requiredProperties = "dialog.requiredProperties";
+  /**
+   * Number of retries for the current Ask.
+   */
 
-  /// Number of retries for the current Ask.
-  static readonly retries = 'dialog.retries'
+  static readonly retries = "dialog.retries";
+  /**
+   * Last intent.
+   */
 
-  /// Last intent.
-  static readonly lastIntent = 'dialog.lastIntent'
+  static readonly lastIntent = "dialog.lastIntent";
+  /**
+   * Last trigger event: defined in FormEvent, ask, clarifyEntity etc.
+   */
 
-  /// Last trigger event: defined in FormEvent, ask, clarifyEntity etc..
-  static readonly lastTriggerEvent = 'dialog.lastTriggerEvent'
+  static readonly lastTriggerEvent = "dialog.lastTriggerEvent";
 }
