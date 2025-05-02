ed258a37c8464567a21d484151c5135774196b17
Jim Daly <jim.daly@microsoft.com>
2025-05-02 13:28:28 -0700

    O365ConnectorCardActionType

--- packages/agents-hosting/src/cards/o365ConnectorCardActionBase.ts
+++ packages/agents-hosting/src/cards/o365ConnectorCardActionBase.ts
@@ -3,7 +3,15 @@
  * Licensed under the MIT License.
  */
 
-export type O365ConnectorCardActionType = 'ViewAction' | 'OpenUri' | 'HttpPOST' | 'ActionCard'
+/**
+ * Defines the possible types of actions in an O365 connector card.
+ *
+ * - `ViewAction`: Represents an action to view content.
+ * - `OpenUri`: Represents an action to open a URI.
+ * - `HttpPOST`: Represents an action to make an HTTP POST request.
+ * - `ActionCard`: Represents an action that opens a card with additional actions or inputs.
+ */
+export type O365ConnectorCardActionType = 'ViewAction' | 'OpenUri' | 'HttpPOST' | 'ActionCard';
 
 /**
  * Represents a base action in an O365 connector card.
