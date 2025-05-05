2d06637930e892920d57e8307c39a33cd7b7a573
Rido <nouser@gh.com>
2025-05-05 23:50:49 +0000

    added first route in extension

--- packages/agents-hosting-extensions-teams/src/myTeamsExtension.ts
+++ packages/agents-hosting-extensions-teams/src/myTeamsExtension.ts
@@ -1,14 +1,20 @@
-import { AgentApplication, AgentExtension, RouteHandler, RouteSelector, TurnState } from '@microsoft/agents-hosting'
-export class MyTeamsExt implements AgentExtension {
-  channelId = 'myTeamsExt'
+import { ActivityTypes } from '@microsoft/agents-activity'
+import { AgentApplication, AgentExtension, RouteHandler, RouteSelector, TurnContext, TurnState } from '@microsoft/agents-hosting'
+import { parseTeamsChannelData } from './activity-extensions/teamsChannelDataParser'
+
+export class MyTeamsExt extends AgentExtension {
   _app: AgentApplication<TurnState>
   constructor (private app: AgentApplication<TurnState>) {
+    super('msteams')
     this._app = app
   }
 
-  addRoute (app: AgentApplication<TurnState>, routeSelector: RouteSelector, routeHandler: RouteHandler<TurnState>, isInvokeRoute: boolean): void {
-    // Add your custom route logic here
-    console.log('Adding route for MyTeamsExt')
-    // app.addRoute(routeSelector, routeHandler)
+  onMessageEdit = (handler: RouteHandler<TurnState>) => {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      const channelData = parseTeamsChannelData(context.activity.channelData)
+      return Promise.resolve(context.activity.type === ActivityTypes.MessageUpdate && channelData && channelData.eventType === 'editMessage')
+    }
+    this.addRoute(this._app, routeSel, handler, false)
+    return this
   }
 }
