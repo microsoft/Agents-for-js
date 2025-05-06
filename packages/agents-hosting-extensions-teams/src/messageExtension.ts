4954e09dfc8261bde20041561bb2102330856e35
Rido <nouser@gh.com>
2025-05-06 21:56:04 +0000

    wip teams extensions

---
+++ packages/agents-hosting-extensions-teams/src/messageExtension.ts
@@ -0,0 +1,244 @@
+import { ActivityTypes } from '@microsoft/agents-activity'
+import { AgentApplication, RouteHandler, RouteSelector, TurnContext, TurnState } from '@microsoft/agents-hosting'
+
+/**
+ * Class that exposes Teams messaging extension-related events.
+ * Provides an organized way to handle messaging extension operations in Microsoft Teams.
+ */
+export class MessageExtension {
+  _app: AgentApplication<TurnState>
+
+  /**
+   * Creates a new instance of the MessageExtension class.
+   * @param app - The agent application
+   */
+  constructor (app: AgentApplication<TurnState>) {
+    this._app = app
+  }
+
+  /**
+   * Handles queries from messaging extensions.
+   * @param handler - The handler to call when a query is received
+   * @returns this (for method chaining)
+   */
+  onQuery (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/query'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true) // Invoke requires true
+    return this
+  }
+
+  /**
+   * Handles link queries from messaging extensions.
+   * @param handler - The handler to call when a link query is received
+   * @returns this (for method chaining)
+   */
+  onQueryLink (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/queryLink'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles anonymous link queries (for public access) from messaging extensions.
+   * @param handler - The handler to call when an anonymous link query is received
+   * @returns this (for method chaining)
+   */
+  onAnonymousQueryLink (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/anonymousQueryLink'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles selection of items from a messaging extension result.
+   * @param handler - The handler to call when an item is selected
+   * @returns this (for method chaining)
+   */
+  onSelectItem (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/selectItem'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles fetch task requests from messaging extensions.
+   * @param handler - The handler to call when a fetch task is requested
+   * @returns this (for method chaining)
+   */
+  onFetchTask (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/fetchTask'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles action submissions from messaging extensions.
+   * @param handler - The handler to call when an action is submitted
+   * @returns this (for method chaining)
+   */
+  onSubmitAction (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/submitAction' // && TODO
+        // (!context.activity.value || !('botMessagePreviewAction' in context.activity.value.))
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles message preview edit actions from messaging extensions.
+   * @param handler - The handler to call when a message preview edit action is received
+   * @returns this (for method chaining)
+   */
+  onMessagePreviewEdit (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(!!(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/submitAction' &&
+        context.activity.value &&
+        // @ts-ignore
+        context.activity.value['botMessagePreviewAction'] === 'edit'))
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles message preview send actions from messaging extensions.
+   * @param handler - The handler to call when a message preview send action is received
+   * @returns this (for method chaining)
+   */
+  onMessagePreviewSend (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(!!(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/submitAction' &&
+        context.activity.value &&
+        // @ts-ignore
+        context.activity.value['botMessagePreviewAction'] === send))
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles configuration query setting URL requests from messaging extensions.
+   * @param handler - The handler to call when a config query setting URL is requested
+   * @returns this (for method chaining)
+   */
+  onConfigurationQuerySettingUrl (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/querySettingUrl'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles configuration setting updates from messaging extensions.
+   * @param handler - The handler to call when configuration settings are updated
+   * @returns this (for method chaining)
+   */
+  onConfigurationSetting (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/setting'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles card button click events from messaging extensions.
+   * @param handler - The handler to call when a card button is clicked
+   * @returns this (for method chaining)
+   */
+  onCardButtonClicked (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/onCardButtonClicked'
+      )
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles app-based link query events.
+   * @param handler - The handler to call when an app-based link query is received
+   * @returns this (for method chaining)
+   */
+  onAppBasedLinkQuery (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(!!(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/queryLink' &&
+        context.activity.value))
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+
+  /**
+   * Handles anonymous app-based link query events.
+   * @param handler - The handler to call when an anonymous app-based link query is received
+   * @returns this (for method chaining)
+   */
+  onAnonymousAppBasedLinkQuery (handler: RouteHandler<TurnState>) {
+    const routeSel: RouteSelector = (context: TurnContext) => {
+      return Promise.resolve(!!(
+        context.activity.type === ActivityTypes.Invoke &&
+        context.activity.channelId === 'msteams' &&
+        context.activity.name === 'composeExtension/anonymousQueryLink' &&
+        context.activity.value))
+    }
+    this._app.addRoute(routeSel, handler, true)
+    return this
+  }
+}
