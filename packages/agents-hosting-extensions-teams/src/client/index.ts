8f7773c78ec7c6ad91292ae6b52450d094476771
Rido <nouser@gh.com>
2025-05-08 00:15:10 +0000

    moving types

---
+++ packages/agents-hosting-extensions-teams/src/client/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './batchFailedEntriesResponse'
+export * from './batchOperationStateResponse'
+export * from './cancelOperationResponse'
+export * from './resourceResponse'
+export * from './teamDetails'
+export * from './teamsBatchOperationResponse'
+export * from './teamsChannelAccount'
+export * from './teamsMember'
+export * from './teamsPagedMembersResult'
+export * from './batchOperationResponse'
+export * from './teamsConnectorClient'
+export * from './batchFailedEntry'
