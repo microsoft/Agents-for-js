a0d10e6de5a12be54d582288ba1611d2e6c2ba8e
Rido <nouser@gh.com>
2025-05-10 00:12:41 +0000

    typeahead

---
+++ samples/basic/typeAheadSearch.ts
@@ -0,0 +1,314 @@
+import axios from 'axios'
+import { startServer } from '@microsoft/agents-hosting-express'
+import { AdaptiveCardSearchResult, AgentApplication, CardFactory, MemoryStorage, MessageFactory, TurnContext, TurnState } from '@microsoft/agents-hosting'
+
+interface packageResult {
+  package: {
+    name: string
+    description: string
+  }
+}
+
+const app = new AgentApplication<TurnState>({ storage: new MemoryStorage() })
+app.conversationUpdate('membersAdded', async (context: TurnContext) => {
+  await context.sendActivity('Hello and welcome! With this sample you can see the functionality of static and dynamic search in adaptive card')
+})
+
+app.adaptiveCards.search('', async (context: TurnContext, state: TurnState) => {
+  const dropdownCard = (context.activity.value! as any).data.choiceselect
+
+  let result
+  if (dropdownCard) {
+    result = getCountrySpecificResults(dropdownCard.toLowerCase())
+  } else {
+    const searchQuery = (context.activity.value! as any).queryText
+    const params = { text: searchQuery, size: 8 }
+    const response = await axios.get('http://registry.npmjs.com/-/v1/search?', { params })
+
+    const npmPackages = response.data.objects.map((obj: packageResult) => ({
+      title: obj.package.name,
+      value: `${obj.package.name} - ${obj.package.description}`
+    }))
+    if (response.status === 200) {
+      result = getSuccessResult(npmPackages)
+    } else if (response.status === 204) {
+      result = getNoResultFound()
+    } else if (response.status === 500) {
+      result = getErrorResult()
+    }
+  }
+  const acResult: AdaptiveCardSearchResult[] = [{
+    title: 'Search Results',
+    value: result
+  }]
+  return Promise.resolve(acResult)
+})
+
+app.activity('message', async (context: TurnContext, state: TurnState) => {
+  const text = context.activity.text?.toLowerCase().trim()
+  const value = context.activity.value
+
+  if (text) {
+    switch (text) {
+      case 'staticsearch':
+        await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(staticSearchCard)))
+        break
+      case 'dynamicsearch':
+        await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(dynamicSearchCard)))
+        break
+      case 'dependantdropdown':
+        await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(dependantSearchCard)))
+        break
+      default:
+        await context.sendActivity("Unknown command. Please use 'staticsearch', 'dynamicsearch', or 'dependantdropdown'.")
+    }
+  } else if (value) {
+    await context.sendActivity(`Selected option is: ${(value as any).choiceselect}`)
+  }
+})
+
+startServer(app)
+
+const staticSearchCard = {
+  $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+  version: '1.2',
+  type: 'AdaptiveCard',
+  body: [
+    {
+      text: 'Please search for the IDE from static list.',
+      wrap: true,
+      type: 'TextBlock'
+    },
+    {
+      columns: [
+        {
+          width: 'auto',
+          items: [
+            {
+              text: 'IDE: ',
+              wrap: true,
+              height: 'stretch',
+              type: 'TextBlock'
+            }
+          ],
+          type: 'Column'
+        }
+      ],
+      type: 'ColumnSet'
+    },
+    {
+      columns: [
+        {
+          width: 'stretch',
+          items: [
+            {
+              choices: [
+                { title: 'Visual studio', value: 'visual_studio' },
+                { title: 'IntelliJ IDEA', value: 'intelliJ_IDEA' },
+                { title: 'Aptana Studio 3', value: 'aptana_studio_3' },
+                { title: 'PyCharm', value: 'pycharm' },
+                { title: 'PhpStorm', value: 'phpstorm' },
+                { title: 'WebStorm', value: 'webstorm' },
+                { title: 'NetBeans', value: 'netbeans' },
+                { title: 'Eclipse', value: 'eclipse' },
+                { title: 'RubyMine', value: 'rubymine' },
+                { title: 'Visual studio code', value: 'visual_studio_code' }
+              ],
+              style: 'filtered',
+              placeholder: 'Search for an IDE',
+              id: 'choiceselect',
+              type: 'Input.ChoiceSet'
+            }
+          ],
+          type: 'Column'
+        }
+      ],
+      type: 'ColumnSet'
+    }
+  ],
+  actions: [
+    {
+      type: 'Action.Submit',
+      id: 'submit',
+      title: 'Submit'
+    }
+  ]
+}
+
+const dynamicSearchCard = {
+  $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+  version: '1.2',
+  type: 'AdaptiveCard',
+  body: [
+    {
+      text: 'Please search for npm packages using dynamic search control.',
+      wrap: true,
+      type: 'TextBlock'
+    },
+    {
+      columns: [
+        {
+          width: 'auto',
+          items: [
+            {
+              text: 'NPM packages search: ',
+              wrap: true,
+              height: 'stretch',
+              type: 'TextBlock'
+            }
+          ],
+          type: 'Column'
+        }
+      ],
+      type: 'ColumnSet'
+    },
+    {
+      columns: [
+        {
+          width: 'stretch',
+          items: [
+            {
+              choices: [
+                { title: 'Static Option 1', value: 'static_option_1' },
+                { title: 'Static Option 2', value: 'static_option_2' },
+                { title: 'Static Option 3', value: 'static_option_3' }
+              ],
+              isMultiSelect: false,
+              style: 'filtered',
+              'choices.data': {
+                type: 'Data.Query',
+                dataset: 'npmpackages'
+              },
+              id: 'choiceselect',
+              type: 'Input.ChoiceSet'
+            }
+          ],
+          type: 'Column'
+        }
+      ],
+      type: 'ColumnSet'
+    }
+  ],
+  actions: [
+    {
+      type: 'Action.Submit',
+      id: 'submitdynamic',
+      title: 'Submit'
+    }
+  ]
+}
+
+const dependantSearchCard = {
+  type: 'AdaptiveCard',
+  $schema: 'https://adaptivecards.io/schemas/adaptive-card.json',
+  version: '1.5',
+  body: [
+    {
+      size: 'ExtraLarge',
+      text: 'Country Picker',
+      weight: 'Bolder',
+      wrap: true,
+      type: 'TextBlock'
+    },
+    {
+      id: 'choiceselect',
+      type: 'Input.ChoiceSet',
+      label: 'Select a country or region:',
+      choices: [
+        { title: 'USA', value: 'usa' },
+        { title: 'France', value: 'france' },
+        { title: 'India', value: 'india' }
+      ],
+      valueChangedAction: {
+        type: 'Action.ResetInputs',
+        targetInputIds: ['city']
+      },
+      isRequired: true,
+      errorMessage: 'Please select a country or region'
+    },
+    {
+      style: 'filtered',
+      'choices.data': {
+        type: 'Data.Query',
+        dataset: 'cities',
+        associatedInputs: 'auto'
+      },
+      id: 'city',
+      type: 'Input.ChoiceSet',
+      label: 'Select a city:',
+      placeholder: 'Type to search for a city in the selected country',
+      isRequired: true,
+      errorMessage: 'Please select a city'
+    }
+  ],
+  actions: [
+    {
+      title: 'Submit',
+      type: 'Action.Submit'
+    }
+  ]
+}
+
+function getCountrySpecificResults (country: 'usa' | 'france' | 'india') {
+  const results = {
+    usa: [
+      { title: 'CA', value: 'CA' },
+      { title: 'FL', value: 'FL' },
+      { title: 'TX', value: 'TX' }
+    ],
+    france: [
+      { title: 'Paris', value: 'Paris' },
+      { title: 'Lyon', value: 'Lyon' },
+      { title: 'Nice', value: 'Nice' }
+    ],
+    india: [
+      { title: 'Delhi', value: 'Delhi' },
+      { title: 'Mumbai', value: 'Mumbai' },
+      { title: 'Pune', value: 'Pune' }
+    ]
+  }
+  // return {
+  //   status: 200,
+  //   body: {
+  //     type: 'application/vnd.microsoft.search.searchResponse',
+  //     value: {
+  //       results: results[country] || results.india
+  //     }
+  //   }
+  // }
+  return results[country] || results.india
+}
+
+function getSuccessResult (npmPackages: packageResult[]) {
+  return {
+    status: 200,
+    body: {
+      type: 'application/vnd.microsoft.search.searchResponse',
+      value: {
+        results: npmPackages
+      }
+    }
+  }
+}
+
+function getNoResultFound () {
+  return {
+    status: 204,
+    body: {
+      type: 'application/vnd.microsoft.search.searchResponse'
+    }
+  }
+}
+
+function getErrorResult () {
+  return {
+    status: 500,
+    body: {
+      type: 'application/vnd.microsoft.error',
+      value: {
+        code: '500',
+        message: 'Error message: Internal Server Error'
+      }
+    }
+  }
+}
