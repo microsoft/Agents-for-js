798562dbde17401a96349209052e77bc03caa1bf
Rido <nouser@gh.com>
2025-05-13 23:06:48 +0000

    config longRunning

--- samples/basic/longRunning.ts
+++ samples/basic/longRunning.ts
@@ -1,12 +1,17 @@
-import { AdaptiveCardActionExecuteResponseType, AgentApplication, CloudAdapter, loadAuthConfigFromEnv, TurnContext, TurnState } from '@microsoft/agents-hosting'
+import { AdaptiveCardActionExecuteResponseType, AgentApplication, CloudAdapter, DefaultConversationState, loadAuthConfigFromEnv, TurnContext, TurnState } from '@microsoft/agents-hosting'
 import { startServer } from '@microsoft/agents-hosting-express'
 
+interface MyUserState {
+  numMessages: number
+}
+
 const adapter = new CloudAdapter(loadAuthConfigFromEnv())
-const app = new AgentApplication<TurnState>({
+const app = new AgentApplication<TurnState<DefaultConversationState, MyUserState>>({
+  // storage: new FileStorage('__state/'),
   adapter,
-  startTypingTimer: true,
-  longRunningMessages: true,
-  adaptiveCards: {
+  startTypingTimer: false,
+  longRunningMessages: false,
+  adaptiveCardsOptions: {
     actionSubmitFilter: 'myFilter',
     actionExecuteResponseType: AdaptiveCardActionExecuteResponseType.NEW_MESSAGE_FOR_ALL
   }
@@ -16,11 +21,13 @@ app.conversationUpdate('membersAdded', async (context: TurnContext) => {
   await context.sendActivity('Welcome to the Echo sample, send a message to see the echo feature in action.')
 })
 
-app.activity('message', async (context: TurnContext, state: TurnState) => {
+app.activity('message', async (context: TurnContext, state: TurnState<DefaultConversationState, MyUserState>) => {
+  const numMessages = state.user.numMessages || 0
   const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
   // await context.sendActivity(`You said1: ${context.activity.text}`)
-  await sleep(5000)
-  await context.sendActivity(`You said2: ${context.activity.text}`)
+  await sleep(3000)
+  await context.sendActivity(`You said [${numMessages}]: ${context.activity.text}`)
+  state.user.numMessages = numMessages + 1
 })
 
 startServer(app)
