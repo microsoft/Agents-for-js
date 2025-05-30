a4d0401645fa02cbbf7c0999666462f47179695e
Rido <nouser@gh.com>
2025-05-30 17:36:33 +0000

    streamingSample

--- samples/basic/streamingSample.ts
+++ samples/basic/streamingSample.ts
@@ -1,31 +1,32 @@
 import { startServer } from '@microsoft/agents-hosting-express'
-import { AgentApplication, MemoryStorage, TurnContext, TurnState } from '@microsoft/agents-hosting'
-import { Activity } from '@microsoft/agents-activity'
+import { AgentApplication, TurnContext, TurnState } from '@microsoft/agents-hosting'
+import { Activity, ActivityTypes } from '@microsoft/agents-activity'
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-const agent = new AgentApplication<TurnState>({ storage: new MemoryStorage() })
+const agent = new AgentApplication<TurnState>()
+
 agent.onConversationUpdate('membersAdded', async (context: TurnContext) => {
   await context.sendActivity('Welcome to the Streaming sample, send a message to see the echo feature in action.')
 })
+
 agent.onActivity('invoke', async (context: TurnContext, state: TurnState) => {
   const invokeResponse = Activity.fromObject({
-    type: 'invokeResponse',
+    type: ActivityTypes.InvokeResponse,
     value: {
       status: 200,
-      body: {
-        message: 'invoke response received'
-      }
     }
   })
   await context.sendActivity(invokeResponse)
 })
+
 agent.onActivity('message', async (context: TurnContext, state: TurnState) => {
   context.streamingResponse.setFeedbackLoop(true)
   context.streamingResponse.setGeneratedByAILabel(true)
+  context.streamingResponse.setSensitivityLabel({ type: 'https://schema.org/Message', '@type': 'CreativeWork', name: 'Internal' })
   await context.streamingResponse.queueInformativeUpdate('starting streaming response')
   await sleep(1000)
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 2; i++) {
     console.log(`Streaming chunk ${i + 1}`)
     await context.streamingResponse.queueTextChunk(`part ${i + 1}`)
     await sleep(1000)
