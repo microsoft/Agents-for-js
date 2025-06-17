dc0d08af47602d65934ded00a67938559caed792
Joel Mut <joel.mut@southworks.com>
2025-06-17 12:17:17 +0100

    Add support for browser esm, comments, and improvements

--- packages/agents-copilotstudio-client/src/index.ts
+++ packages/agents-copilotstudio-client/src/index.ts
@@ -4,14 +4,25 @@ import { CopilotStudioWebChat } from './copilotStudioWebChat'
 export * from './agentType'
 export * from './connectionSettings'
 export * from './copilotStudioClient'
-export * from './directToEngineConnectionSettings'
+export * from './copilotStudioConnectionSettings'
+export * from './copilotStudioWebChat'
 export * from './executeTurnRequest'
 export * from './powerPlatformCloud'
 export * from './powerPlatformEnvironment'
 
-// Define CopilotStudioClient on globalThis for browsers
+// Define CopilotStudioClient on window object for browsers
+declare global {
+  interface Window {
+    Agents?: {
+      CopilotStudioClient: typeof CopilotStudioClient;
+      CopilotStudioWebChat: typeof CopilotStudioWebChat;
+    };
+  }
+}
+
 if (typeof window !== 'undefined') {
-  (globalThis as any).Agents = {
+  window.Agents = {
+    ...(window.Agents ?? {}),
     CopilotStudioClient,
     CopilotStudioWebChat,
   }
