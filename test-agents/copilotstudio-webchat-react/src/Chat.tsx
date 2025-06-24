941e69f92526532a523a57e07f330d4a205959f0
Kyle Rohn <t-kylerohn@microsoft.com>
2025-06-23 20:22:17 -0700

    add msft headers

--- test-agents/copilotstudio-webchat-react/src/Chat.tsx
+++ test-agents/copilotstudio-webchat-react/src/Chat.tsx
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { Components } from 'botframework-webchat'
 import { FluentThemeProvider } from 'botframework-webchat-fluent-theme'
 import React, { useState, useEffect } from 'react'
