ad4c325bd21dff6ff611637b10b99b2b1f993be4
Tracy Boehrer <trboehre@microsoft.com>
2025-10-10 08:33:04 -0500

    Sample fixes

--- samples/basic/headerPropagation.ts
+++ samples/basic/headerPropagation.ts
@@ -21,7 +21,7 @@ echo.onConversationUpdate('membersAdded', async (context: TurnContext) => {
 echo.onActivity('message', async (context: TurnContext, state: TurnState) => {
   const headersReceived = Object.entries(incomingHeaders)
     .map(([key, value]) => `- **${key}**: ${value}`).join('\n  ')
-  const headersSent = Object.entries((context.adapter as CloudAdapter).connectorClient.axiosInstance.defaults.headers)
+  const headersSent = Object.entries(context.turnState.get('ConnectorClient').axiosInstance.defaults.headers)
     .filter(([_, e]) => typeof e === 'string')
     .map(([key, value]) => `- **${key}**: ${value}`).join('\n  ')
 
